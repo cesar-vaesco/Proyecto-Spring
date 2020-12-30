ac0179e044bcb7954a91542820d50a160e87236f
Cesar-Dev <cesarvargasescorcia@gmail.com>
2020-12-30 17:15:57 -0600

    @PathVariable: crear la URL con pasando uno o más  parametros 'variables'

---
+++ src/main/java/com/practicavaesco/springboot/web/app/controllers/EjemploVariablesRutaController.java
@@ -0,0 +1,33 @@
+package com.practicavaesco.springboot.web.app.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/variables")
+public class EjemploVariablesRutaController {
+
+	@GetMapping({"/","index","inicio",""})
+	public String index(Model model) {
+		model.addAttribute("titulo", "Enviar parámetros de la ruta - (@PathVariable)");
+		return "variables/index";
+	}
+
+	//http://localhost:8200/variables/string/cualquiercosa
+	@GetMapping("/string/{texto}")
+	public String variables(@PathVariable String texto, Model model) {
+		model.addAttribute("titulo", "Recibir parámetros de la ruta - (@PathVariable)");
+		model.addAttribute("resultado", "El texto enviado en la ruta es: " + texto);
+		return "variables/ver";
+	}
+	//http://localhost:8200/variables/string/cualquiercosa
+		@GetMapping("/string/{texto}/{numero}")
+		public String variables(@PathVariable String texto,@PathVariable Integer numero , Model model) {
+			model.addAttribute("titulo", "Recibir parámetros de la ruta - (@PathVariable)");
+			model.addAttribute("resultado", "El texto enviado en la ruta es: " + texto + " y el número enviado en el path es: "+ numero);
+			return "variables/ver";
+		}
+}
