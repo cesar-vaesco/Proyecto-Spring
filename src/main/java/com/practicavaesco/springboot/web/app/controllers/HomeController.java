d4f9a4f0d30718848afd3a69d3f4f3759b4cb0dd
Cesar-Dev <cesarvargasescorcia@gmail.com>
2020-12-30 19:16:05 -0600

    'Forward:'redirigir vistas dentro de nuestra app

--- src/main/java/com/practicavaesco/springboot/web/app/controllers/HomeController.java
+++ src/main/java/com/practicavaesco/springboot/web/app/controllers/HomeController.java
@@ -8,6 +8,7 @@ public class HomeController {
 
 	@GetMapping("/")
 	public String home() {
-		return "redirect:/app/index";
+		return "forward:/app/index";
+		//return "redirect:https://www.google.com/";
 	}
 }
